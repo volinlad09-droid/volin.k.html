e9da32611d814dd8fb29ea57aad2bcaeb378bf51
mr volin <volin.lad09@gmail.com>
2026-08-14 19:52:31 +0530

    Add files via upload

---
+++ 1.1.py
@@ -0,0 +1,67 @@
+# Python Basics
+'''
+print("Hello , My name is MR k.")
+'''
+# Add two numbers
+'''
+a = 5
+b = 3
+'''
+# a - variable name
+# = : - Assignement Operator
+# 5 : - value
+'''
+sum = a + b
+
+print("Sum : " , sum)
+'''
+# Ask User's Name
+"""
+name = input("What is your name?")
+
+print("Nice to meet you , " , name)
+
+a = input("Enter first number : ")
+b = input("Enter second number:")
+
+sum = int(a) + int(b)  # type conversion
+
+print(sum)
+
+"""
+
+# Simple Calculator
+'''
+a = int(input("Enter first number : "))
+b = int(input("Enter second number:"))
+
+print("Addition : " , a + b)
+print("Substraction :" , a - b)
+print("Multiplication : " , a * b)
+print("Division : " , a / b)
+print("Expontiations : " , a ** b)
+'''
+# pet talk Simulator
+
+pet = input("what pet do you have (dog / cat / fish) : ")
+
+# Control Flow Statements
+
+# if...elif...else statement
+
+if pet == "dog":
+    print("bhow bhow! I want to sepcial treatment.")
+elif pet == "cat":
+    print("meow meow , I am special...")
+elif pet == "fish":
+    print("Blub blub , I am swimming in style.")
+else :
+    print("That's a unique pet.")
+
+
+
+
+
+
+
+
